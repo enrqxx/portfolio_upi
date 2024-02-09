986a8e2ff680ea2f0300996623e4f73a71f27cbe
enrqxx <enrqx001@gmail.com>
2024-02-09 21:23:31 +0700

    New File PDF CV

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/upi-rapih.jpg";
-import CV from "../../assets/Smith-Cv.pdf";
+import CV from "../../assets/Muhammad Yusyfi Fuada-CV.rar";
 import Info from "./Info";
 
 const About = () => {
