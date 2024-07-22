97a968b263a3c02542edbef29e8707f6064bdfa6
enrqxx <enrqx001@gmail.com>
2024-07-22 10:43:11 +0700

    Commit Pertama

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/upi-rapih.jpg";
-import CV from "../../assets/Muhammad Yusyfi Fuada-CV.rar";
+import CV from '../../assets/MuhammadYusyfiFuada-CV.pdf';
 import Info from "./Info";
 
 const About = () => {
--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -50,74 +50,26 @@ const Qualification = () => {
           >
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">Web Design</h3>
+                <h3 className="qualification__title">
+                Informatics Engineering</h3>
                 <span className="qualification__subtitle">
-                  Bandung <br /> Universitas Pasundan
-                </span>
-                <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2018 - Present
-                </div>
-              </div>
-
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div>
-
-            <div className="qualification__data">
-              <div></div>
-
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-
-              <div>
-                <h3 className="qualification__title">Web Development</h3>
-                <span className="qualification__subtitle">
-                  Bandung <br />
-                  Universitas Pasundan
-                </span>
-                <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2019 - 2023
-                </div>
-              </div>
-            </div>
-
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title">Project With Prof</h3>
-                <span className="qualification__subtitle">
-                  Bandung <br /> Universitas Pasundan
-                </span>
-                <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - 2023
-                </div>
-              </div>
-
-              <div>
-                <span className="qualification__rounder"></span>
-                <span className="qualification__line"></span>
-              </div>
-            </div>
-
-            <div className="qualification__data">
-              <div></div>
-
-              <div>
-                <span className="qualification__rounder"></span>
-              </div>
-
-              <div>
-                <h3 className="qualification__title">College Graduation</h3>
-                <span className="qualification__subtitle">
-                  Bandung <br /> Universitas Pasundan
+                  Bandung - University Pasundan
                 </span>
                 <div className="qualification__calender">
                   <i className="uil uil-calendar-alt"></i> 2018 - 2023
                 </div>
               </div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+            </div>
+
+            <div className="qualification__data">
+              <div></div>
+
+
             </div>
           </div>
 
@@ -130,15 +82,14 @@ const Qualification = () => {
           >
             <div className="qualification__data">
               <div>
-                <h3 className="qualification__title">Web Development</h3>
+                <h3 className="qualification__title">Freelance Web Development</h3>
                 <span className="qualification__subtitle">
-                  Purwakarta <br /> Pratama Solusi Teknologi
+                  Purwakarta | Pratama Solusi Teknologi
                 </span>
                 <div className="qualification__calender">
-                  <i className="uil uil-calendar-alt"></i> 2021 - 2022
+                  <i className="uil uil-calendar-alt"></i> 3 Month
                 </div>
               </div>
-              
 
               <div>
                 <span className="qualification__rounder"></span>
@@ -150,23 +101,22 @@ const Qualification = () => {
               <div></div>
 
               <div>
-                <h3 className="qualification__title"></h3>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">Freelance Web Development</h3>
                 <span className="qualification__subtitle">
-                  
+                  Pangandaran - IT Elementary School An-Nahar
                 </span>
                 <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 2 Month
                 </div>
               </div>
             </div>
 
-            <div className="qualification__data">
-              <div>
-                <h3 className="qualification__title"></h3>
-                <span className="qualification__subtitle"></span>
-                <div className="qualification__calender">
-                </div>
-              </div>
-            </div>
+
           </div>
         </div>
       </div>
