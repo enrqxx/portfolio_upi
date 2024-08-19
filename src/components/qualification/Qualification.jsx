36825094f2577bea8eaa25f8a3cc37de610ce268
enrqx <enrqx001@gmail.com>
2024-08-19 11:26:27 +0700

    Update CV dan Qualification

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -133,6 +133,27 @@ const Qualification = () => {
               </div>
             </div>
 
+              {/* LEFT */}
+              
+            <div className="qualification__data">
+              <div></div>
+
+              <div>
+                <span className="qualification__rounder"></span>
+                <span className="qualification__line"></span>
+              </div>
+
+              <div>
+                <h3 className="qualification__title">IT Programmer</h3>
+                <span className="qualification__subtitle">
+                  RSU Asri - Purwakarta
+                </span>
+                <div className="qualification__calender">
+                  <i className="uil uil-calendar-alt"></i> 3 Month
+                </div>
+              </div>
+            </div>
+
 
 
 
